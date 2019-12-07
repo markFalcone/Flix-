d205aeaa46b5a71a18ceda4b55ada9d6766be937
markFalcone <falconemark16@yahoo.com>
2019-12-06 22:26:49 -0800

    Add files via upload

---
+++ MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Mark Falcone on 12/6/19.
+//  Copyright © 2019 Mark Falcone. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
